2f423b4211a2f9f0f6f5fe35c98f652499b65e7a
Ogunsakin Damilola <33791201+OgunsakinDamilola@users.noreply.github.com>
2022-08-27 09:28:13 +0100

    added more test

--- tests/Unit/InterswitchPaymentModelTest.php
+++ tests/Unit/InterswitchPaymentModelTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace OgunsakinDamilola\Interswitch\Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -9,16 +10,37 @@ class InterswitchPaymentModelTest extends TestCase
 {
     use RefreshDatabase;
 
+    public $interswitchPayment;
+
     public function setUp(): void
     {
         parent::setUp();
+        $this->setInterswitchPayment();
     }
 
     /** @test */
     public function model_can_be_initiated_with_factory()
     {
-        $model = factory(InterswitchPayment::class)->create();
+        $this->assertTrue($this->interswitchPayment instanceof InterswitchPayment);
+    }
 
-        $this->assertTrue($model instanceof InterswitchPayment);
+    /** @test */
+    public function model_has_relevant_properties(){
+        $model = $this->interswitchPayment->toArray();
+        $this->assertArrayHasKey('id', $model);
+        $this->assertArrayHasKey('customer_id', $model);
+        $this->assertArrayHasKey('customer_name', $model);
+        $this->assertArrayHasKey('customer_email', $model);
+        $this->assertArrayHasKey('environment', $model);
+        $this->assertArrayHasKey('gateway', $model);
+        $this->assertArrayHasKey('reference', $model);
+        $this->assertArrayHasKey('amount', $model);
+        $this->assertArrayHasKey('response_code', $model);
+        $this->assertArrayHasKey('response_description', $model);
+    }
+
+    private function setInterswitchPayment()
+    {
+        $this->interswitchPayment = factory(InterswitchPayment::class)->create();
     }
 }
